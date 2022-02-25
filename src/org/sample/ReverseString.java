b7f56c4dac34483ed6f0c99be37af3d78819456c
vinoviji <r.vinoviji434@gmail.com>
2022-02-25 18:10:45 +0530

    done

---
+++ src/org/sample/ReverseString.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String s1="vinoth";
+		String s2="";
+		for (int i = s1.length()-1; i>=0; i--) {
+			char ch = s1.charAt(i);
+			s2=s2+ch;
+			
+			
+		}
+		System.out.println(s2);
+	}
+
+}
